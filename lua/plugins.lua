f8a4c6b49e0b28de513073bf1cf9980ee0058a2e
la1den <lowfhon@outlook.com>
2023-07-30 17:52:39 +0800

    add plugin
    distant - remote edit plugin
    lualine - neovim statusline plugin
    surround - edit quotes with ease
    lazygit - pluging for calling lazygit from within neovim
    toggleterm - help easily manage multiple termianl windows

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,68 +2,6 @@ return require('packer').startup(function()
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-    -- Simple plugins can be specified as strings
-    -- use '9mm/vim-closer'
-
-    -- Lazy loading:
-    -- Load on specific commands
-    -- use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
-
-    -- Load on an autocommand event
-    -- use {'andymass/vim-matchup', event = 'VimEnter'}
-
-    -- Load on a combination of conditions: specific filetypes or commands
-    -- Also run code after load (see the "config" key)
-    -- use {
-    -- 	'w0rp/ale',
-    -- 	ft = {'sh', 'zsh', 'bash', 'c', 'cpp', 'cmake', 'html', 'markdown', 'racket', 'vim', 'tex'},
-    -- 	cmd = 'ALEEnable',
-    -- 	config = 'vim.cmd[[ALEEnable]]'
-    -- }
-
-    -- Plugins can have dependencies on other plugins
-    -- use {
-    -- 	'haorenW1025/completion-nvim',
-    -- 	opt = true,
-    -- 	requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
-    -- }
-
-    -- -- Plugins can also depend on rocks from luarocks.org:
-    -- use {
-    --	'my/supercoolplugin',
-    --	rocks = {'lpeg', {'lua-cjson', version = '2.1.0'}}
-    --}
-
-    -- You can specify rocks in isolation
-    -- use_rocks 'penlight'
-    -- -- use_rocks {'lua-resty-http', 'lpeg'}
-
-    -- -- -- Local plugins can be included
-    -- -- -- use '~/projects/personal/hover.nvim'
-
-    -- -- -- Plugins can have post-install/update hooks
-    -- -- use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'}
-
-
-    -- -- Post-install/update hook with call of vimscript function with argument
-    -- -- use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
-
-    -- -- -- use {
-    -- -- 	'glepnir/galaxyline.nvim',
-    -- -- 	branch = 'main',
-    -- -- 	-- your statusline
-    -- -- 	-- some optional icons
-    -- 	requires = {'kyazdani42/nvim-web-devicons', opt = true}
-    -- }
-    -- Use dependency and run lua function after load
-    -- use {
-    -- 	'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
-    -- 	config = function() require('gitsigns').setup() end
-    -- }
-
-    -- You can specify multiple plugins in a single call
-    -- use {'tjdevries/colorbuddy.vim', {'nvim-treesitter/nvim-treesitter', opt = true}}
-
     -- You can alias plugin names
     use {'dracula/vim', as = 'dracula'}
 
@@ -76,18 +14,6 @@ return require('packer').startup(function()
     -- True snippet and additional text editing support
     use {'neoclide/coc.nvim', branch = 'release'}
 
-    -- snippets engine
-    -- use {'SirVer/ultisnips'}
-
-    -- a collection of snippets
-    -- use {'honza/vim-snippets'}
-
-    -- a file explorer for neovim(netrw comes as default for neovim)
-    -- use {'scrooloose/nerdtree'}
-
-    -- an easy way for commenting out lines
-    -- use {'preservim/nerdcommenter'}
-    --
 
     -- A file explorer tree for neovim written in lua
     use {
@@ -102,34 +28,15 @@ return require('packer').startup(function()
     use {'mhinz/vim-startify'}
 
     -- fzf is a general-purpose command-line fuzzy finder.
-    -- use {'junegunn/fzf'}
-    -- use {'junegunn/fzf.vim'}
     use { 'ibhagwan/fzf-lua' }
-
-
-    -- TypeScript Highlighting
-    -- use {'leafgarland/typescript-vim'}
-    -- use {'peitalin/vim-jsx-typescript'}
 
     -- browse the tags of the current file and get an overview of its structure
     use {'majutsushi/tagbar'}
-
-    -- takes care of the much needed management of tags files in Vim.
-    -- use {'ludovicchabant/vim-gutentags'}
-
-    -- Vim plugin, insert or delete brackets, parens, quotes in pair
-    -- use {'jiangmiao/auto-pairs'}
 
     -- Vim plugin for intensely nerdy commenting powers
     -- use {'preservim/nerdcommenter'}
     use {'tpope/vim-commentary'}
 
-    -- autopairs for neovim written by lua
-    -- use {
-    --     "windwp/nvim-autopairs",
-    --     config = function() require("nvim-autopairs").setup {} end
-    -- }
-    --
 
     -- install without yarn or npm
     use({
@@ -138,6 +45,44 @@ return require('packer').startup(function()
     })
 
     use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
+
+    use {
+        'chipsenkbeil/distant.nvim',
+        config = function()
+            require('distant').setup {
+                -- Applies Chip's personal settings to every machine you connect to
+                --
+                -- 1. Ensures that distant servers terminate with no connections
+                -- 2. Provides navigation bindings for remote directories
+                -- 3. Provides keybinding to jump into a remote file's parent directory
+                ['*'] = require('distant.settings').chip_default()
+            }
+        end
+    }
+
+    -- A blazing fast and easy to configure neovim statusline plugin written in pure lua.
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    
+    -- surround.vim: Delete/change/add parentheses/quotes/XML-tags/much more with ease
+    use { 'tpope/vim-surround' }
+
+    -- Plugin for calling lazygit from within neovim.
+    use({
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
+    })
+
+    -- A neovim lua plugin to help easily manage multiple terminal windows
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+        require("toggleterm").setup()
+    end}
 end)
 
 
