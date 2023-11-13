9c64aa7c26779911779f8c74d0123f5dd0f04252
la1den <lowfhon@outlook.com>
2023-11-13 09:14:17 +0800

    add help tags map

--- lua/user/fzf-lua.lua
+++ lua/user/fzf-lua.lua
@@ -1,1 +1,4 @@
-vim.api.nvim_set_keymap('n', '<c-P>', ':<C-U>lua require("fzf-lua").files()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<c-P>', '<cmd>lua require("fzf-lua").files()<CR>', {noremap = true})
+
+vim.api.nvim_set_keymap('n', '<F1>', '<cmd>FzfLua help_tags<CR>', {noremap = true})
+vim.api.nvim_set_keymap('i', '<F1>', '<Esc><cmd>FzfLua help_tags<CR>', {noremap = true})
