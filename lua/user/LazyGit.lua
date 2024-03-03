86187c6d7111dda7435b7067f799625faf7fc638
la1den <lowfhon@outlook.com>
2024-03-03 20:56:47 +0800

    update fzf-lua and lazygit

--- lua/user/LazyGit.lua
+++ lua/user/LazyGit.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_set_keymap('n', '<leader>gg', ':<C-U>LazyGit<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>lg', ':<C-U>LazyGit<CR>', {noremap = true, silent = true})
--- lua/user/fzf-lua.lua
+++ lua/user/fzf-lua.lua
@@ -8,10 +8,12 @@ require'fzf-lua'.setup {
 
 }
 vim.api.nvim_set_keymap('n', '<c-P>', '<cmd>lua require("fzf-lua").files()<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>s', '<cmd>lua require("fzf-lua").live_grep_glob()<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>/', '<cmd>lua require("fzf-lua").lgrep_curbuf()<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>gp', '<cmd>lua require("fzf-lua").grep_project()<CR>', {noremap = true})
 
+-- grep uses rg (or grep if rg is not installed) to search for a pattern, the results are then fed into fzf for further refinement using fuzzy search.
+-- live_grep on the other hand uses fzf as a UI selector only, the fuzzy search functionality is disabled and each keystroke generates a new rg query with the input prompt text.
+vim.api.nvim_set_keymap('n', '<leader>sf', '<cmd>lua require("fzf-lua").grep_project()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>sr', '<cmd>lua require("fzf-lua").live_grep_glob()<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>/', '<cmd>lua require("fzf-lua").lgrep_curbuf()<CR>', {noremap = true})
 
 vim.api.nvim_set_keymap('n', '<F1>', '<cmd>FzfLua help_tags<CR>', {noremap = true})
 vim.api.nvim_set_keymap('i', '<F1>', '<Esc><cmd>FzfLua help_tags<CR>', {noremap = true})
